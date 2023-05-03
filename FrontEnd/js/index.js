2d4ebc5a2520a0e684a583648a28b36ab3a2becf
NicolasBeckerich <beckerichnicolas.com>
2023-05-03 10:42:46 +0100

    réorganisation du css + création index.css et login.css + optimisation index.js

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,36 +1,42 @@
 fetch("http://localhost:5678/api/categories")
   .then(response => response.json())
-  .then(projects => {
-    projects.forEach(project => {
+  .then(categories => {
 
-      const input = document.createElement("input");
-      input.setAttribute("type", "button");
-      input.setAttribute("data-categories", project.id);
-      input.setAttribute("value", project.name);
-      input.setAttribute("class", "Tous");
-      const divfiltres = document.querySelector("#filtres");
-      divfiltres.appendChild(input);
-
-
+    //Céer l'élèment id 0 name Tous
+    categories.unshift({
+      id : 0,
+      name : "Tous"
     });
+    
+    console.log(categories);
+    categories.forEach(category => {
 
-    // Création constante correspondant récupération html via class = Tous
-    const boutonFiltres = document.querySelectorAll(".Tous");
-    // Création boucle sur Bouton Filtre pour récuperer tout les clique
-    boutonFiltres.forEach(function (boutonFiltre) {
+      const span = document.createElement("span");
+      span.setAttribute("data-categories", category.id);
+      span.innerText = category.name;
+      span.classList.add("Tous");
+
+      const divfiltres = document.querySelector("#filtres");
+      divfiltres.appendChild(span);
+
+
       //Création Evènement pour récupération des cliques e=evenement
-      boutonFiltre.addEventListener("click", function (e) {
+      span.addEventListener("click", function (e) { //-------------------------------------------------Remplacer e par event
+        
+        // Création constante correspondant récupération html via class = Tous
+        const boutonFiltres = document.querySelectorAll(".Tous");
+
         // Remove les classes cliqué de chaque élément de mes boutons
         boutonFiltres.forEach(boutonFiltre => {
-          boutonFiltre.classList.remove("test");
+          boutonFiltre.classList.remove("filtres-color");
 
         });
         // Ajout classes css pour avoir le vert au clique avec le target
-        e.target.classList.add("test");
+        e.target.classList.add("filtres-color");
         // création variable target afin d'aller chercher dans le Dataset de l'html la valeur correspondante
         let target = e.target.dataset.categories;
         //appelle de la fonction test avec paramètres target déclaré précedemment
-        Test(target);
+        Tri(target);
       });
     });
   })
@@ -39,42 +45,40 @@ fetch("http://localhost:5678/api/categories")
 
 
 // Appelle de la fonction sans filtres par la valeur donné au data-set Tous
-Test("0");
-// function Test(target = "0") option alternative par défaut paramètre fonction = "0"
+Tri(0);
+// function Test(target = 0) option alternative par défaut paramètre fonction = 0
 /**
  * Création fonction permettant d'utiliser le fetch important les travaux dans la galerie et de filtrer en fonction de la category.Id
  * @param {string} target 
  */
-function Test(target) {
+function Tri(target) {
   fetch("http://localhost:5678/api/works")
     .then(response => response.json())
     .then(projects => {
+
       //vider les éléments de la gallery et les recharger selon les filtres
       document.querySelector(".gallery").innerHTML = "";
+
       // condition permettant de filter selon mes catégories et le target
-      if (target != "0") {
-        const filtreCategories = projects.filter(function (project) {
+      if (target > 0) {
+        projects = projects.filter(function (project) {
           return project.categoryId == target;
         });
-        // Redéclaration de valeur de porjects pour qu'il corresponde au tableau filtré
-        projects = filtreCategories;
       }
-
-       projects = Array.from(new Set(projects.map(a => a.id)))
-        .map(id => {
-          return projects.find(a => a.id === id)
-        })
-
+      
       projects.forEach(project => {
 
         const figure = document.createElement("figure");
+
+        //création élémént img
         const img = document.createElement("img");
         img.setAttribute("src", project.imageUrl);
         img.setAttribute("alt", project.title);
+
         const figcaption = document.createElement("figcaption");
-        figcaption.textContent = project.title;
+        figcaption.innerText = project.title;
 
-
+        //remplacement depuis api vers html
         const divGallery = document.querySelector(".gallery");
         divGallery.appendChild(figure);
         figure.appendChild(img);
--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,1 +1,15 @@
-alert("Oui");
+let user = {
+    name: 'John',
+    surname: 'Smith'
+  };
+  
+  let response = await fetch('/article/fetch/post/user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(user)
+  });
+  
+  let result = await response.json();
+  alert(result.message);
